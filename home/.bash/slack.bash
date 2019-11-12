9478108c4ef72844c6d0296ccd7af434fac7fce7
Kolbe Kegel <kolbe@kolbekegel.com>
2019-11-11 16:24:15 -0800

    Some random updates

--- home/.bash/slack.bash
+++ home/.bash/slack.bash
@@ -2,5 +2,10 @@
 # TiDB Community (kolbe)
 tidb_slack_user_count(){
     num=$( slack_fetch_list users | grep -Ev 'pingcap.com|slackbot' | wc -l )
-    box 5 "TiDB Community Slack has $(( num )) users!"
+    if [[ -t 1 ]]; then
+        cmd=(box 5)
+    else
+        cmd=(echo)
+    fi
+    "${cmd[@]}" "TiDB Community Slack has $(( num )) users!"
 }
